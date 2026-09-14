1eb7d85f897e8fb1e4ce833bb2a8604529b7990f
Leo Lin <leolinuk25@gmail.com>
2026-09-14 14:47:39 +0100

    feat: started on model.py
    train a model pipeline

---
+++ src/model.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import Pipeline
+
+def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> Pipeline:
+    
+
+    return pipeline
