26f59638d626f141f02391e69fe821c0a60879d4
Leo Lin <leolinuk25@gmail.com>
2026-09-14 21:26:33 +0100

    feat: added model
    train and evaluate the model functions

--- src/model.py
+++ src/model.py
@@ -1,8 +1,29 @@
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, log_loss, roc_auc_score
 
+# function to create a pipeline for the data and train the model
 def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> Pipeline:
-    
+    pipeline = Pipeline([
+        ('scalar', StandardScaler()),
+        ('classifier', LogisticRegression(max_iter=1000, random_state=42))
+    ])
 
+    pipeline.fit(X_train, y_train)
     return pipeline
+
+# evaluate the model and return a dict with performance metrics
+def evaluate_model(model: Pipeline, X_test: pd.DataFrame, y_test: pd.DataFrame) -> dict:
+    y_pred = model.predict(X_test)
+    y_prob = model.predict_proba(X_test)[:, 1]
+
+    metrics = {
+        'Accuracy': accuracy_score(y_test, y_pred),
+        'ROC-AUC': roc_auc_score(y_test, y_prob), # area under the ROC curve 
+        'Log-Loss': log_loss(y_test, y_prob)
+    }
+
+    return metrics
